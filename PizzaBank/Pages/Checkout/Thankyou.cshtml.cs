b686a960a75d09a514ba9f3abfcee0f75cb7eea0
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-27 21:54:18 -0800

    init pizza bank

---
+++ PizzaBank/Pages/Checkout/Thankyou.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaBank.Pages.Checkout
+{
+	public class ThankyouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
