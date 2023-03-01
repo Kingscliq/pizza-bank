2d04fb5f4eda577d6a10af1ef90c7dcc5bce39ab
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-28 22:06:59 -0800

    Creation of the pizza

---
+++ PizzaBank/Models/PizzasModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PizzaBank.Models
+{
+	public class PizzasModel
+	{
+		public string ImageTitle { get; set; }
+		public string PizzaName { get; set; }
+		public float BasePrice { get; set; }
+
+		public bool TomatoeSauce { get; set; }
+
+        public bool Beef { get; set; }
+        public bool Peperoni { get; set; }
+        public bool MushRoom { get; set; }
+        public bool Tuna { get; set; }
+		public bool Pineapple { get; set; }
+
+		public float TotalPrice { get; set; }
+
+    
+    }
+}
+
--- PizzaBank/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzaBank/Pages/Forms/CustomPizza.cshtml.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaBank.Models;
 
 namespace PizzaBank.Pages.Forms
 {
-	public class IndexModel : PageModel
+    public class IndexModel : PageModel
     {
+        public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
+     
         public void OnGet()
         {
+        }
+
+        public IActionResult onPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.Beef) PizzaPrice += 1;
+            if (Pizza.TomatoeSauce) PizzaPrice += 1;
+            if (Pizza.Peperoni) PizzaPrice += 1;
+            if (Pizza.Pineapple) PizzaPrice += 10;
+            if (Pizza.MushRoom) PizzaPrice += 3;
+            if (Pizza.Tuna) PizzaPrice += 1;
+
+            return RedirectToPage("/checkout/index", new {Pizza.PizzaName, PizzaPrice });
+
         }
     }
 }
